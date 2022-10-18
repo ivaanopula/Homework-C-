1f8cc75cf6fe75790829b25bdf2523d8e1246c2c
Ivaanopula __ <ivaanopula@gmail.com>
2022-10-18 20:55:51 +0300

    Добавил файл .csproj в гитигнор

---
+++ Example_2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+
+Console.WriteLine("Введите первое число.");
+int num1 = int.Parse (Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int num2 = int.Parse(Console.ReadLine()!);
+if (num1>num2)
+{
+    Console.WriteLine("Большее число = " + num1);
+}
+else {
+    Console.WriteLine("Большее число = " + num2);
+}
---
+++ Example_4/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+
+Console.WriteLine("Введите число 1.");
+int num1 = int.Parse (Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int num2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число");
+int num3 = int.Parse(Console.ReadLine()!);
+ if (num1 > num2 && num1 > num3)
+Console.WriteLine("Nubmer Max = " + num1); 
+  else if (num2 > num3)
+Console.WriteLine("Number Max = " + num2); 
+  else
+Console.WriteLine("Number Max = " + num3); 
